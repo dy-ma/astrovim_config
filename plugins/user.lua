e78837b9d06a681fdf4e4b7afd53c89afca6d849
Dylan Ang <73457638+stack-dylan@users.noreply.github.com>
2023-04-21 06:30:54 -0700

    Add my used languages

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,11 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    -- config = function()
+    --   require("todo-comments").setup {}
+    opts = {}
+  }
 }
